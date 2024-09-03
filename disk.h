cc31e0ad2adbb8d45532f0dc619ec11b1d013e48
erpaffo <116151302+erpaffo@users.noreply.github.com>
2024-09-03 16:30:20 +0200

    Add FAT and Disk structures

---
+++ disk.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define DISK_SIZE 1024*1024 // 1MB
+#define BLOCK_SIZE 512 // 512B
+#define FAT_SIZE DISK_SIZE / BLOCK_SIZE
+
+// Struttura per entry nella FAT
+typedef struct {
+    int next_block;
+    int file; 
+} FatEntry;
+
+// struttura File Allocation Table (FAT)
+typedef struct {
+    int free_blocks; 
+    FatEntry entries[FAT_SIZE]; 
+} Fat;
+
+typedef struct {
+    int size; // Dimensione disco
+    Fat fat; // File Allocation Table per disco
+    char data[]; // Dati del disco
+} Disk;
